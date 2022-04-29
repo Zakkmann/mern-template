9da3721ee5e6b92738cbdb20c01272fd652eada6
zakkmann <zakkman@yandex.ru>
2022-04-29 18:54:47 +0300

    front

--- client/config/root.jsx
+++ client/config/root.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const Root = () => {
-   return (<div>This is Root, muthafucka!</div>)
+   return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>This is React Router v6</div>} />
+        <Route path="/test" element={<div>This is Test Route!</div>} />
+        <Route path="/*" element={<h3>Не туда! Уходи!</h3>} />
+      </Routes>
+    </BrowserRouter>
+   )
 }
-
 export default Root 
